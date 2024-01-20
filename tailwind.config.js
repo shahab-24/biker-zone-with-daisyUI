652d5159ef70ff2392e7d0facd287a61c1b1abe1
Shahab Uddin <shahab.82946@gmail.com>
2024-01-20 14:31:58 +0600

    added navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  //...
-  plugins: [require("daisyui")],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
